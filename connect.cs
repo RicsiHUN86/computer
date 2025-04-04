ef514dc28fb947052919976bc8d0adff88a8f8ce
TaskoEniko <taskoe@kkszki.hu>
2025-04-04 12:01:37 +0200

    login, exit done

--- connect.cs
+++ connect.cs
@@ -4,7 +4,7 @@ using System.Data;
 
 public class Connect
 {
-    private const string connectionString = "server=localhost;database=computer;user=root;password=;";
+    private const string connectionString = "server=localhost;database=computer;user=root;password=password;";
 
     public static MySqlConnection GetConnection()
     {
